15274194e6173ce6ec79eb23db6d08dbaac4c7b9
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-07 01:31:26 +0300

    Created UserForRegisterDto.cs

---
+++ Entities/DTO/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+namespace Entities.DTO
+{
+    public class UserForRegisterDto
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string StudentNumber { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
