cab57dd7c9c61b4b0374bbce888baba81e6a0560
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-07 01:03:00 +0300

    Blog class created.

--- Entities/Concrete/Blog.cs
+++ Entities/Concrete/Blog.cs
@@ -4,6 +4,7 @@ namespace Entities.Concrete
     {
         public int BlogId { get; set; }
         public int UserId { get; set; }
+        public string Title { get; set; }
         public string Content { get; set; }
         public string ImageUrl { get; set; }
         public int LikeCount { get; set; }
