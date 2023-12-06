5fcd6fe73d1aa77533719caac5a4e002b0d5bd25
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-07 01:06:29 +0300

    Ctf class created.

---
+++ Entities/Concrete/Ctf.cs
@@ -0,0 +1,12 @@
+namespace Entities.Concrete
+{
+    public class Ctf{
+        public int CtfId { get; set; }
+        public string Title { get; set; }
+        public string Question { get; set; }
+        public string QuestionImageUrl { get; set; }
+        public string Answer { get; set; }
+        public int Point { get; set; }
+        public int NumberOfSolve { get; set; }
+    }
+}
