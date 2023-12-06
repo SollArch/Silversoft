c8ed6d86f631b5c501079bb84e8111bfa0801262
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-07 01:30:54 +0300

    Created ChangePasswordDto.cs

---
+++ Entities/DTO/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+namespace Entities.DTO
+{
+    public class ChangePasswordDto
+    {
+        public int UserId { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
