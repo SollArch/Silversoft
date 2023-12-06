b9a7c6b1a7f78b787fdf8d2eba79e44fcd18c761
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-07 00:46:56 +0300

    User class modified for requirements

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -1,20 +1,18 @@
-﻿using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Core.Entities.Concrete
+﻿namespace Core.Entities.Concrete
 {
     public class User : IEntity
     {
         public int UserId { get; set; }
+        public string UserName { get; set; }
+        
+        public string StudentNumber { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public bool Status { get; set; }
-        public int FindexPoint { get; set; }
+        public int Point { get; set; }
 
     }
 }
