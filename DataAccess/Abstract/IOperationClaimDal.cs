4db3aeec4b1f97fead2dda603e0f65b6d2845894
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-09 04:06:11 +0300

    Update IOperationClaimDal.cs

--- DataAccess/Abstract/IOperationClaimDal.cs
+++ DataAccess/Abstract/IOperationClaimDal.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using Core.DataAccess;
+using Core.Entities.Concrete;
+using DataAccess.Context;
+
 namespace DataAccess.Abstract
 {
-    public interface IOperationClaimDal
+    public interface IOperationClaimDal : IEntityRepository<OperationClaim>
     {
-        
+        List<OperationClaim> GetClaims(int userId);
     }
 }
