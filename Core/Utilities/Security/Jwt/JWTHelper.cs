c80706a0e481317c9a19cf1d42a180013c0460a4
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-09 04:06:03 +0300

    Update JWTHelper.cs

--- Core/Utilities/Security/Jwt/JWTHelper.cs
+++ Core/Utilities/Security/Jwt/JWTHelper.cs
@@ -11,12 +11,12 @@ using System.Security.Claims;
 
 namespace Core.Utilities.Security.Jwt
 {
-    public class JWTHelper : ITokenHelper
+    public class JwtHelper : ITokenHelper
     {
         public IConfiguration Configuration { get; }
         private TokenOptions _tokenOptions;
         private DateTime _accessTokenExpiration;
-        public JWTHelper(IConfiguration configuration)
+        public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
             _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
