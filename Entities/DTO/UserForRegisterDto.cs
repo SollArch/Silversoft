e0dee963fa2bc1e711f7ad8bf7d04a8eca5de281
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-09 04:06:30 +0300

    Update UserForRegisterDto.cs

--- Entities/DTO/UserForRegisterDto.cs
+++ Entities/DTO/UserForRegisterDto.cs
@@ -2,13 +2,12 @@ using Core.Entities.Abstract;
 
 namespace Entities.DTO
 {
-    public class UserForRegisterDto : IDto
+    public abstract class UserForRegisterDto : IDto
     {
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string StudentNumber { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
