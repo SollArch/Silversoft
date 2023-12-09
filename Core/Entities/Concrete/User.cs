7053b5f195821e68fcf40b65f04ee16982a4f6a2
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-09 04:05:57 +0300

    Update User.cs

--- Core/Entities/Concrete/User.cs
+++ Core/Entities/Concrete/User.cs
@@ -11,11 +11,12 @@ namespace Core.Entities.Concrete
         public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string StudentNumber { get; set; }
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public int Point { get; set; }
         public bool Status { get; set; }
-        public int FindexPoint { get; set; }
-
     }
 }
