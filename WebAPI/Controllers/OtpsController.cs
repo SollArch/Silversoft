93c2cdfbbe4a531e3799602cbc9a2a1011e793a5
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:13:33 +0300

    Create OtpsController.cs

---
+++ WebAPI/Controllers/OtpsController.cs
@@ -0,0 +1,34 @@
+using Business.Abstract;
+using Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OtpsController : Controller
+{
+    private readonly IOtpService _otpService;
+    private readonly IAuthService _authService;
+    public OtpsController(IOtpService otpService, IAuthService authService)
+    {
+        _otpService = otpService;
+        _authService = authService;
+    }
+    
+    [HttpPost("checkotp")]
+    public IActionResult CheckOtp([FromBody] CheckOtpDto checkOtpDto)
+    {
+        var otpResult = _otpService.CheckOtp(checkOtpDto);
+        if (!otpResult.Success)
+        {
+            return BadRequest(otpResult.Message);
+        }
+        var result = _authService.CreateAccessToken(otpResult.Data);
+        if (result.Success)
+        {
+            return Ok(result.Data);
+        }
+        return BadRequest(otpResult.Message);
+    }
+}
