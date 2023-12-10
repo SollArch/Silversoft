d1d1364e887b5c2dbfce5b761d4ef8875977f0ad
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:17:05 +0300

    New messages added and never used mesage removed

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -5,12 +5,26 @@ namespace Business.Constants
         // User
         public const string UserDeleted = "Kullanıcı silindi.";
         public const string UserUpdated = "Kullanıcı güncellendi.";
-        
+        public const string UserWasBlocked = "Hesabınız askıya alınmış görünüyor. Bir hata olduğunu düşünüyorsanız lütfen iletişime geçiniz.";
+        public const string PasswordSendToYourMail = "Giriş şifreniz mail adresinize gönderilmiştir. Lütfen kontrol ediniz.";
         
         // User Rules
         public const string ThisEmailAlreadyExists = "Bu e-posta adresi zaten mevcut.";
         public const string ThisUserNameAlreadyExists = "Bu kullanıcı adı zaten mevcut.";
         public const string ThisStudentNumberAlreadyExists = "Bu öğrenci numarası zaten mevcut.";
+        
+        
+        // Operation Claim
+        public const string OperationClaimDeleted = "Yetki silindi.";
+        public const string OperationClaimUpdated = "Yetki güncellendi.";
+        public const string OperationClaimAdded = "Yetki eklendi.";
+        public const string ThisOperationClaimNameAlreadyExists = "Bu yetki adı zaten mevcut.";
+        
+        
+        // User Operation Claim
+        public const string UserOperationClaimAdded = "Kullanıcı yetkisi eklendi.";
+        public const string UserOperationClaimDeleted = "Kullanıcı yetkisi silindi.";
+        public const string UserOperationClaimUpdated = "Kullanıcı yetkisi güncellendi.";
         
 
         // Password Validation
@@ -24,6 +38,11 @@ namespace Business.Constants
         public const string UserNotFound = "Kullanıcı bulunamadı.";
         public const string AuthorizationDenied = "Yetkiniz yok.";
         public const string UserRegistered = "Kullanıcı kayıt edildi.";
-        public const string AccessTokenCreated = "Access token oluşturuldu.";
+        
+        // Otp
+        public const string OtpNotFound = "Otp bulunamadı.";
+        public const string OtpExpired = "Otp süresi geçmiş.";
+        public const string OtpNotMatch = "Otp eşleşmiyor.";
+        public const string OtpSended = "Otp gönderildi.";
     }
 }
