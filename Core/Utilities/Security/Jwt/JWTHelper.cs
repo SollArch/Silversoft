bab7228595321a867cd07e549acb66f65e48a894
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:19:52 +0300

    IConfigration field's access modifiers changed to private and TokenOptions field signed to readonly

--- Core/Utilities/Security/Jwt/JWTHelper.cs
+++ Core/Utilities/Security/Jwt/JWTHelper.cs
@@ -13,8 +13,8 @@ namespace Core.Utilities.Security.Jwt
 {
     public class JwtHelper : ITokenHelper
     {
-        public IConfiguration Configuration { get; }
-        private TokenOptions _tokenOptions;
+        private IConfiguration Configuration { get; }
+        private readonly TokenOptions _tokenOptions;
         private DateTime _accessTokenExpiration;
         public JwtHelper(IConfiguration configuration)
         {
