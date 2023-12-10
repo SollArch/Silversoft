b6c43333192cc155878dd450d5ad476a355568cb
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:14:53 +0300

    The filed of Surname changed to LastName

--- Entities/DTO/UserForRegisterDto.cs
+++ Entities/DTO/UserForRegisterDto.cs
@@ -2,12 +2,14 @@ using Core.Entities.Abstract;
 
 namespace Entities.DTO
 {
-    public abstract class UserForRegisterDto : IDto
+    public class UserForRegisterDto : IDto
     {
         public string FirstName { get; set; }
-        public string Surname { get; set; }
+        public string LastName { get; set; }
         public string StudentNumber { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        
+        
     }
 }
