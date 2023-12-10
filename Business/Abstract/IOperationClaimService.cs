1453021a04a7f10d413bad27ed7f60f51d84eae5
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:08:08 +0300

    Update IOperationClaimService.cs

--- Business/Abstract/IOperationClaimService.cs
+++ Business/Abstract/IOperationClaimService.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
 using Core.Entities.Concrete;
+using Core.Utilities.Results;
 
 namespace Business.Abstract
 {
     public interface IOperationClaimService
     {
-        List<OperationClaim> GetClaims(int userId);
+
+        IResult Add(OperationClaim operationClaim);
+        IResult Update(OperationClaim operationClaim);
+        IResult Delete(OperationClaim operationClaim);
+        IDataResult<OperationClaim> GetById(int operationClaimId);
+        IDataResult<OperationClaim> GetByName(string operationClaimName);
+        IDataResult<List<OperationClaim>> GetAll(); 
+        
     }
 }
