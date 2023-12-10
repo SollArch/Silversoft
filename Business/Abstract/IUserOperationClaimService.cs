5d50a85246dd8447df62ffcb9f82d3f44c598f01
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:08:25 +0300

    Create IUserOperationClaimService.cs

---
+++ Business/Abstract/IUserOperationClaimService.cs
@@ -0,0 +1,14 @@
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+
+namespace Business.Abstract
+{
+    public interface IUserOperationClaimService
+    {
+        IResult Add(UserOperationClaim userOperationClaim);
+        IResult Update(UserOperationClaim userOperationClaim); 
+        IDataResult<UserOperationClaim> GetById(int userOperationClaimId);
+        IDataResult<UserOperationClaim> GetByUserId(int userId);
+
+    }
+}
