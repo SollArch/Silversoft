7469451db42c163cf022a67659145d450f933fd5
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-10 15:16:40 +0300

    New Table Added to Context And ConnectionString Changed

--- DataAccess/Context/DatabaseContext.cs
+++ DataAccess/Context/DatabaseContext.cs
@@ -8,15 +8,17 @@ namespace DataAccess.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=MFBILGIN\MFBILGIN;SilversoftDatabase=Test;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(
+                connectionString: @"Server=MFBILGIN\MFBILGIN;Database=silversoft;Trusted_Connection=true");
         }
 
         public DbSet<User> Users { get; set; }
-        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }  
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Ctf> Ctfs { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Prize> Prizes { get; set; }
+        public DbSet<Otp> Otps { get; set; }
     }
 }
