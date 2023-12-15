1637462001483e4bd5d97ee7bdbdb287aecf98b8
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-15 03:34:15 +0300

    Update UsersController.cs

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Business.Abstract;
 using Core.Entities.Concrete;
 using Entities.DTO.Get;
+using Entities.DTO.Post.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApPI.Controllers;
@@ -12,95 +13,80 @@ public class UsersController : Controller
 {
     private readonly IUserService _userService;
     private readonly IMapper _mapper;
+
     public UsersController(IUserService userService, IMapper mapper)
     {
         _userService = userService;
         _mapper = mapper;
     }
-    
-    [HttpGet("getall")]
-    public IActionResult GetAll()
-    {
-        var result = _userService.GetAll();
-        if (result.Success)
-        {
-            return Ok(result.Data);
-        }
-        return BadRequest(result.Message);
-    }
-    
+
+
     [HttpGet("getbyid/{id}")]
     public IActionResult GetById([FromRoute] int id)
     {
         var result = _userService.GetById(id);
-        if (result.Success)
-        {
-            return Ok(result.Data);
-        }
-        return BadRequest(result.Message);
+        if (!result.Success) return BadRequest(result.Message);
+        var userGetDto = _mapper.Map<UserGetDto>(result.Data);
+        return Ok(userGetDto);
     }
-    
+
     [HttpGet("getbyusername/{userName}")]
     public IActionResult GetByUserName([FromRoute] string userName)
     {
         var result = _userService.GetByUserName(userName);
-        if (result.Success)
-        {
-            return Ok(result.Data);
-        }
-        return BadRequest(result.Message);
+        if (!result.Success) return BadRequest(result.Message);
+        var userGetDto = _mapper.Map<UserGetDto>(result.Data);
+        return Ok(userGetDto);
     }
-    
+
     [HttpGet("getbystudentnumber/{studentNumber}")]
     public IActionResult GetByStudentNumber([FromRoute] string studentNumber)
     {
         var result = _userService.GetByStudentNumber(studentNumber);
-        if (result.Success)
-        {
-            return Ok(result.Data);
-        }
-        return BadRequest(result.Message);
+        if (!result.Success) return BadRequest(result.Message);
+        var userGetDto = _mapper.Map<UserGetDto>(result.Data);
+        return Ok(userGetDto);
     }
-    
+
     [HttpGet("getbyemail/{email}")]
     public IActionResult GetByEmail([FromRoute] string email)
     {
         var result = _userService.GetByEmail(email);
-        
-        if (result.Success)
-        {
-            var userGetDto = _mapper.Map<UserGetDto>(result.Data);
-            return Ok(userGetDto);
-        }
-        return BadRequest(result.Message);
+
+        if (!result.Success) return BadRequest(result.Message);
+        var userGetDto = _mapper.Map<UserGetDto>(result.Data);
+        return Ok(userGetDto);
     }
-    
+
     [HttpGet("getclaims/{userId}")]
     public IActionResult GetClaims([FromRoute] int userId)
     {
         var result = _userService.GetClaims(userId);
-            return Ok(result);
+        return Ok(result);
     }
 
-    [HttpPost("update")]
-    public IActionResult Update([FromBody] User user)
+    [HttpPut("update")]
+    public IActionResult Update([FromBody] UserForUpdate userFor)
     {
+        var user = _mapper.Map<User>(userFor);
         var result = _userService.Update(user);
         if (result.Success)
         {
-            return Ok(result.Message);
+            return Ok(result);
         }
-        return BadRequest(result.Message);
+
+        return BadRequest(result);
     }
-    
-    [HttpPost("delete")]
-    public IActionResult Delete([FromBody] User user)
+
+    [HttpDelete("delete")]
+    public IActionResult Delete([FromBody] UserForDelete userForDelete)
     {
-        var result = _userService.Delete(user);
+        var result = _userService.Delete(userForDelete);
         if (result.Success)
         {
-            return Ok(result.Message);
+            return Ok(result);
         }
-        return BadRequest(result.Message);
+
+        return BadRequest(result);
     }
 }
