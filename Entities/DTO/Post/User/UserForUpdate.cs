b82451b7ec16f2c89d7c45c758aadbcb3aef2fe9
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2023-12-15 03:34:28 +0300

    Create UserForUpdate.cs

---
+++ Entities/DTO/Post/User/UserForUpdate.cs
@@ -0,0 +1,11 @@
+namespace Entities.DTO.Post.User;
+
+public class UserForUpdate
+{
+    public int UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string StudentNumber { get; set; } 
+    public string Email { get; set; } 
+}
